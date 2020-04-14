e4acbe645701820209b04725bcd0a74259719620
Douglas Fernandes <douglasf.filho@gmail.com>
2020-04-14 13:10:02 -0300

    add: sample kotlin spring boot application

---
+++ src/main/kotlin/br/com/douglasffilho/springkotlincrud/SpringKotlinCrudApplication.kt
@@ -0,0 +1,11 @@
+package br.com.douglasffilho.springkotlincrud
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringKotlinCrudApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringKotlinCrudApplication>(*args)
+}
---
+++ src/test/kotlin/br/com/douglasffilho/springkotlincrud/SpringKotlinCrudApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.douglasffilho.springkotlincrud
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringKotlinCrudApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
